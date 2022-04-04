3e206a596b0f2d1e0b031269d058fa79206645c5
LeastKIds <leastkids@gmail.com>
2022-04-04 09:12:02 +0900

    https -> http allow

--- studion_application_swiftUI/repository/ServerAdress.swift
+++ studion_application_swiftUI/repository/ServerAdress.swift
@@ -11,21 +11,24 @@ public class ServerAdress {
     static let sharedInstance = ServerAdress()
     
 //    let api: String = "http://172.21.2.52"
-    let api: String = "http://192.168.1.4"
+//    let api: String = "http://192.168.1.4"
 //    let api: String = "http://172.20.10.3"
 //    let api: String = "http://172.21.2.201"
 //    let api: String = "http://192.168.0.78"
 //    let api: String = "http://192.168.38.71"
-//    let api: String = "https://api.liveroom.shop/"
+    let api: String = "https://api.liveroom.shop/"
+    let socketApi: String = "https://socket.liveroom.shop/"
     
     
     func getApiServerAdress() -> String {
-        return api + ":8000/"
+//        return api + ":8000/"
+        return api
     }
     
     func getSocketServerAdress() -> String {
 //        return api + ":5000/"
-        return api + ":9876/"
+//        return api + ":9876/"
+        return socketApi
     }
     
     
