23a3e8c24d0aa49cd9b16c1e578d690e4ef22855
ChiKim22 <wlsgh2505@gmail.com>
2022-05-16 20:41:39 +0900

    Studion_App(+CurrentTime, Duration)

--- studion_application_swiftUI/page/Post/AudioView.swift
+++ studion_application_swiftUI/page/Post/AudioView.swift
@@ -15,7 +15,14 @@ struct AudioView : View {
     @State var player = AVPlayer()
     @State var playerItem: AVPlayerItem?
     @State var isPlaying: Bool = false
-    @State var isEnded: Bool = false
+    @State var value: Double = 0
+    
+    
+    //time format
+    @State var time: Double = 0
+    @State var duration: Double = 0
+    @State var cntTimeString = ""
+    @State var durationString = ""
     
     var body: some View {
             
@@ -29,11 +36,24 @@ struct AudioView : View {
                     
                     Capsule().fill(Color.black.opacity(0.08)).frame(height: 8)
                     Capsule().fill(Color.green).frame(width: self.width, height: 8)
+                        .gesture(DragGesture()
+                            .onChanged({ (value) in
+                                let x = value.location.x
+//                                self.width = geometry.size.width - 30
+//                                let percent = x / self.width
+//                                var cntTime = player.currentItem!.currentTime()
+//                                let floatDuration = player.currentItem!.asset.duration
+
+                                
+//                                time = Double(percent) * duration
+                                
+                            }))
 
                 }
             
                 HStack(spacing: geometry.size.width / 5 - 30) {
-                
+                    Text(cntTimeString)
+                    Spacer()
                 
                     Button(action: {}, label: {
                         Image(systemName: self.isPlaying ? "pause.fill" : "play.fill").font(.title)
@@ -42,66 +62,98 @@ struct AudioView : View {
                             
                             player.pause()
                             self.isPlaying = false
-                            
-                        } else if isEnded {
-                            isEnded = false
-                            player.play()
-                            self.isPlaying = true
 
                         } else {
+                            
                             player.play()
                             self.isPlaying = true
+                            
                         }
                     }// onTab (touch event for button)
+                    
+                    Spacer()
+                    Text(durationString)
                 
             } // hS
             .padding(.top)
             .foregroundColor(Color.black)
-            .task() {
+            .onAppear() {
                     guard let url = URL(string: (audioURL)!) else {
                         print("wrong url")
                         return
                     }
-                    if isEnded {
-                        playerItem = nil
-                        player.replaceCurrentItem(with: playerItem)
-                    } else {
                         playerItem = AVPlayerItem(url: url)
                         player.replaceCurrentItem(with: playerItem)
-                    }
                 
                 
-                    Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (_) in
+                    Timer.scheduledTimer(withTimeInterval: 1 / 60, repeats: true) { (_) in
 //                            print(audioURL)
                         
                         if isPlaying == true {
+                            
+//                            if isEnded {
+//                                time = 0
+//                                value = 0
+//                                isEnded = false
+//                            }
         
-                            let time = CMTimeGetSeconds(player.currentTime())
-                            let duration = CMTimeGetSeconds(player.currentItem!.duration)
-                            let floatDuration = Float64(duration)
+                            
+                            time = CMTimeGetSeconds(player.currentTime())
+                            
+                            if time.isNaN || time.isInfinite || time == 0.0 {
+                                return
+                            } else {
+                                let totalCntTime = Int(time)
+                                let tMin = totalCntTime / 60
+                                let tSec = totalCntTime % 60
+                                let tString = String(format: "%02d:%02d", tMin, tSec)
+                                self.cntTimeString = tString
+                            }
+                            
+                            duration = CMTimeGetSeconds(player.currentItem!.duration)
+                            
+                            if duration.isNaN || duration.isInfinite || duration == 0.0 {
+                                return
+                            } else {
+                                let totalDuration = Int(duration)
+                                let dMin = totalDuration / 60
+                                let dSec = totalDuration % 60
+                                let dString = String(format: "%02d:%02d", dMin, dSec)
+                                self.durationString = dString
+                            }
                             
                             let screen = geometry.size.width - 30
-                            let value = time / floatDuration
+                            value = time / duration
                             
                             
-                            print(screen)
+//                            print(screen)
+//                            print("Value!!!!! : \(value)")
+//                            print("Time!!!!!!! : \(round(time.value()))")
+//                            print("Duration!!!!!!! : \(round(duration.value()))")
+
                             
                                 if value < 1 {
                                     self.width = screen * CGFloat(value)
                                 } else {
                                     self.width = screen + 30
-                                    isEnded = true
+//                                    isEnded = false
                                     isPlaying = false
                                 }
-                            
-                            print("Value!!!!! : \(value)")
-                            print("Time!!!!!!! : \(time)")
-                            print("Duration!!!!!!! : \(duration)")
 
-                        }
+                        } // time
                     }
                 
-                }// task
+                }// onAppear
+                .onDisappear() {
+//                    self.time = 0
+//                    self.duration = 0
+//                    self.width = 0
+                    self.durationString = ""
+                    self.cntTimeString = ""
+                    self.isPlaying = false
+                    player.pause()
+//                    player.replaceCurrentItem(with: nil)
+                }
         
             } // vs
             
@@ -110,7 +162,6 @@ struct AudioView : View {
         
     }// view
 }
-
 
 
 //struct AudioView_Previews: PreviewProvider {
